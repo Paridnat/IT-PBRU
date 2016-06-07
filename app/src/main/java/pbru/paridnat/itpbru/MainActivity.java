a54a785edc41b22fce1b0c851b9084938f3d6fd6
FilmParidnat <paridnatfilm@gmail.com>
2016-06-07 15:52:34 +0700

    Send data to database

--- app/src/main/java/pbru/paridnat/itpbru/MainActivity.java
+++ app/src/main/java/pbru/paridnat/itpbru/MainActivity.java
@@ -17,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
         myManage = new MyManage(this);
 
+        //Test Add New User
+        //myManage.addNewUser("123", "name", "sur", "user", "pass"); test send data to database
+
     } // Main Method
 
     public void clickSignUpMain(View view) {
