7a2c87d8daa71ec3c512baf0e3172fcae6f732bf
FilmParidnat <paridnatfilm@gmail.com>
2016-06-07 11:02:57 +0700

    Check space of Alert in SignUpActivity page

--- app/src/main/java/pbru/paridnat/itpbru/SignUpActivity.java
+++ app/src/main/java/pbru/paridnat/itpbru/SignUpActivity.java
@@ -1,13 +1,63 @@
 package pbru.paridnat.itpbru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    // Explicit ประกาศตัวแปร
+    private EditText nameEditText, surnameEditText, userEditText, passwordEditText;
+    private String nameString, surnameString, userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget การผูกตัวแปร
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surnameEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+
     }
+
+    public void clickSignUpSign(View view) {
+        //getText() รับค่าจากเครื่องมือ
+        //toString() รับมาแล้วเปลี่ยนค่าที่ได้เป็น String
+        //trim() ตัดค่าว่างที่อาจจะแถมมาจากเครื่องมือ
+        nameString = nameEditText.getText().toString().trim();
+        surnameString = surnameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (checkSpace()) {
+
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องคะ");
+
+        } else {
+
+
+
+        }
+
+    }
+
+    private boolean checkSpace() {
+
+        boolean result = true;
+
+        result = nameString.equals("") || surnameString.equals("") ||
+                userString.equals("") || passwordString.equals("");
+
+        return result;
+    }
+
+
 }
